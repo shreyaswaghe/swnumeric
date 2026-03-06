0211162a1ea78a3b411c550f29eb0c7a6de830a5
Shreyas Waghe <shreyaswaghe3@gamil.com>
2026-03-05 22:05:53 -0500

    convenience routines for Vector

--- library/vectormatrix/Vector.h
+++ library/vectormatrix/Vector.h
@@ -1,23 +1,35 @@
 #pragma once
 
+#include <mkl_cblas.h>
+#include <mkl_cblas_64.h>
+#include <sys/cdefs.h>
+
 #include <cassert>
+#include <cmath>
 #include <concepts>
 #include <cstddef>
 #include <cstring>
+#include <type_traits>
 #include <vector>
 
 #include "library/expression/Expression.h"
 
 template <size_t N, std::floating_point T>
-struct StaticVector : Expr<StaticVector<N, T>, T> {
+struct StaticVector
+// : Expr<StaticVector<N, T>, T>
+{
   static_assert(N > 0, "Length to StaticVector must be positive.");
 
   //
   using value_type = T;
+  constexpr static size_t ctime_size = N;
+  //
   T _data[N];
 
   // size
   constexpr size_t size() const noexcept { return N; }
+  constexpr bool is_alloc() const noexcept { return true; }
+  constexpr bool alloc(size_t /*N*/) const noexcept { return true; }
 
   // data access
   constexpr T* data() noexcept { return _data; }
@@ -48,17 +60,160 @@ struct StaticVector : Expr<StaticVector<N, T>, T> {
   }
 };
 
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator+(const StaticVector<N, T>& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] + b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator-(const StaticVector<N, T>& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] - b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator*(const StaticVector<N, T>& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] * b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator/(const StaticVector<N, T>& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] / b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator+(const StaticVector<N, T>& a,
+                                             const T& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] + b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator-(const StaticVector<N, T>& a,
+                                             const T& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] - b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator*(const StaticVector<N, T>& a,
+                                             const T& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] * b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator/(const StaticVector<N, T>& a,
+                                             const T& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a[i] / b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator+(const T& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a + b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator-(const T& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a - b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator*(const T& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a * b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator/(const T& a,
+                                             const StaticVector<N, T>& b) {
+  StaticVector<N, T> c;
+  for (size_t i = 0; i < N; i++) c[i] = a / b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T>& operator+=(StaticVector<N, T>& a,
+                                               const StaticVector<N, T>& b) {
+  for (size_t i = 0; i < N; i++) a[i] += b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator-=(StaticVector<N, T>& a,
+                                              const StaticVector<N, T>& b) {
+  for (size_t i = 0; i < N; i++) a[i] -= b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator*=(StaticVector<N, T>& a,
+                                              const StaticVector<N, T>& b) {
+  for (size_t i = 0; i < N; i++) a[i] *= b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator/=(StaticVector<N, T>& a,
+                                              const StaticVector<N, T>& b) {
+  for (size_t i = 0; i < N; i++) a[i] /= b[i];
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T>& operator+=(StaticVector<N, T>& a,
+                                               const T& b) {
+  for (size_t i = 0; i < N; i++) a[i] += b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator-=(StaticVector<N, T>& a,
+                                              const T& b) {
+  for (size_t i = 0; i < N; i++) a[i] -= b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator*=(StaticVector<N, T>& a,
+                                              const T& b) {
+  for (size_t i = 0; i < N; i++) a[i] *= b;
+}
+
+template <size_t N, std::floating_point T>
+__always_inline StaticVector<N, T> operator/=(StaticVector<N, T>& a,
+                                              const T& b) {
+  for (size_t i = 0; i < N; i++) a[i] /= b;
+}
+
 template <std::floating_point T>
 struct Vector : Expr<Vector<T>, T> {
   //
   using value_type = T;
+  constexpr static size_t ctime_size = 0;
+
+  //
   std::vector<T> _data;
 
   // constructor
   explicit Vector(size_t N) : _data(N) {}
+  explicit Vector() {}
 
   // size
   constexpr size_t size() const noexcept { return _data.size(); }
+  constexpr bool is_alloc() const noexcept { return size() > 0; }
+  constexpr bool alloc(size_t N) {
+    if (is_alloc()) [[unlikely]]
+      return false;
+    _data.resize(N);
+    return true;
+  }
 
   // data access
   constexpr T* data() noexcept { return _data.data(); }
@@ -130,3 +285,46 @@ constexpr auto operator/(const V1& a, const V2& b) {
   assert(a.size() == b.size());
   return BinaryExpr<V1, V2, Div, typename V1::value_type>{a, b};
 }
+
+// other common operators
+
+template <size_t N, typename T>
+T norm2(const StaticVector<N, T>& x) {
+  if constexpr (std::is_same_v<T, double>) {
+    cblas_dnrm2(N, x.data(), 1);
+  } else if constexpr (std::is_same_v<T, float>) {
+    cblas_snrm2(N, x.data(), 1);
+  } else {
+    T nrm{};
+    for (size_t i = 0; i < x.size(); i++) {
+      nrm += x[i] * x[i];
+    }
+    return std::sqrt(nrm);
+  }
+}
+
+template <typename T>
+T norm2(const Vector<T>& x) {
+  if constexpr (std::is_same_v<T, double>) {
+    cblas_dnrm2(x.size(), x.data(), 1);
+  } else if constexpr (std::is_same_v<T, float>) {
+    cblas_snrm2(x.size(), x.data(), 1);
+  } else {
+    T nrm{};
+    for (size_t i = 0; i < x.size(); i++) {
+      nrm += x[i] * x[i];
+    }
+    return std::sqrt(nrm);
+  }
+}
+
+// some wrapper types
+#define SIZE_CONDITIONED_VECTOR_T \
+  std::conditional_t<(N > 0), StaticVector<N, T>, Vector<T>>  // no-semi!
+
+// vector with possibly custom vector norm
+template <size_t N = 0, std::floating_point T = double>  //
+struct NormedVector {
+  SIZE_CONDITIONED_VECTOR_T v;
+  T (*norm)(const SIZE_CONDITIONED_VECTOR_T& x) = norm2;
+};
